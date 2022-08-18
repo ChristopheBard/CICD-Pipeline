dd0392253d95e7702e02c5bce7a8cb88800dd4a8
Christophe BARD <christophebard96@gmail.com>
2022-08-18 13:05:55 +0000

    6th commit for demo

--- Build/pong.js
+++ Build/pong.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/ping', function(req, res) {
     console.log("received");
     res.setHeader('Content-Type', 'text/plain');
-    res.end("PONG from build #5");
+    res.end("PONG from build #6");
 });
 app.listen(80);
